107ac88fe306aae2c12c596abfbc56cd750c32c5
Iryna Sokolovska <sokirina777@gmail.com>
2023-12-13 10:50:23 +0200

    fix four: виправлено

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -29,11 +29,12 @@ const signup = async (req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    const avatarURL = gravatar.url(email);
     if (user) {
       throw HttpError(409, "Email in use");
     }
+
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
     const verificationToken = nanoid();
 
@@ -74,7 +75,7 @@ const verify = async (req, res, next) => {
 
     await User.findByIdAndUpdate(user._id, {
       verify: true,
-      verificationToken: "",
+      verificationToken: " ",
     });
 
     res.json({
